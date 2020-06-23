ad637b966250c4cbc1e899f4c0143148c5c3d7da
fatimashahid101 <64368003+fatimashahid101@users.noreply.github.com>
2020-06-23 05:13:05 -0700

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,58 @@
+// // task 3
+// for (var i = 0; i <= 10; i++ ){
+//     document.write(i + "<br>")
+// }
+
+// // task 4
+// var table = prompt("Number of table")
+// var multiplication = prompt("How much multiplication you want")
+// for(var i = 1; i <= multiplication ; i++ ){
+//     document.write(table + " X " + i + " = " + table*i + "<br>")
+// }
+
+// // task 5 
+
+// fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+// for(var i = 0; i < fruits.length ; i++){
+//     document.write(fruits[i] + "<br>")
+    
+// }
+// for(var i = 0; i < fruits.length ; i++){
+//     document.write("Element at index " + i + " is " + fruits[i] + "<br>")
+    
+// }
+
+// // task 6
+// var counting_arr = new Array() 
+// for ( var i = 0 ; i <= 10; i++){
+//     counting_arr.push(i)
+// }
+// document.write("<h1> Counting </h1> <br>" + counting_arr + "<br>")
+
+// var reverse_arr = new Array() 
+// for ( var i = 10 ; i >= 0 ; i--){
+//     reverse_arr.push(i)
+// }
+// document.write("<h1> Reverse </h1> <br>" + reverse_arr + "<br>")
+
+// var odd_arr = new Array()
+// for ( var i = 1 ; i <= 20 ; i += 2){
+//     odd_arr.push(i)
+// }
+// document.write("<h1> Odd </h1> <br>" + odd_arr + "<br>")
+
+// var series_arr = new Array()
+// for(var i = 2; i <= 20; i += 2){
+//     series_arr.push(i + "k")
+// } 
+// document.write("<h1> Series </h1> <br>" + series_arr + "<br>")
+
+// // task 7
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var B = prompt("Welcome to ABC Bakery,What do you want to order Sir/Ma'am?")
+// for ( i = 0; i < A.length ; i++){
+//     if ( A[i] === B) {
+//         document.write(A[i] + " is availabe at Index " + i + " in our bakery")
+   
+// }
+// }
